800b617e12d72ba0d64a74dbe90223977ff155c2
yevhen31546 <dobryanskiy.yevhen@gmail.com>
2020-03-10 06:31:46 +0200

    backend endpoint setting

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var salesRouter = require('./routes/sales');
+var warrantyRouter = require('./routes/warranty');
 
 var app = express();
 
@@ -20,7 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/sales', salesRouter);
+app.use('/warranty', warrantyRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
